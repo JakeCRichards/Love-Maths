ef07d0f9ba882a73b0ee51b61bfcd59dcbda4870
JakeCRichards <j.c.delaney.92@gmail.com>
2025-01-29 13:17:17 +0000

    Set up structure of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("Connected!")
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer()  {
+
+}
+
+function incrementScore()   {
+
+}
+
+function incremenetWrongAnswer()    {
+
+}
+
+function displayAdditionQuestion()  {
+
+}
+
+function displaySubtractionQuestion()  {
+
+}
+
+function displayMultiplyQuestion()  {
+
+}
